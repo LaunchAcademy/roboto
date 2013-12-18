f5c258b363d8a93bd0dcb991957fef6da5ad1a5f
Dan Pickett <dan.pickett@launchware.com>
2013-12-17 20:41:38 -0500

    omit test help from the spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-require "rails/test_help"
+# require "rails/test_help"
 
 require 'capybara/rspec'
 require 'rspec/rails'
